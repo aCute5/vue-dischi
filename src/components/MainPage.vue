b670aefdd68efa530033c709d18c1f7b34c07d39
aCute5 <costantini95dario@gmail.com>
2022-11-28 19:30:05 +0100

    added icon loading

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="container-fluid">
-    <div class="row">
+    <div v-if="arrDiscs" class="row">
       <CardDisc
         v-for="Objdisc in arrDiscs"
         :key="Objdisc.poster"
         :disc="Objdisc"
       ></CardDisc>
+    </div>
+    <div v-else class="loading">
+      <i class="fa-solid fa-spinner"></i>
     </div>
   </div>
 </template>
@@ -45,5 +48,18 @@ export default {
 .row {
   width: 800px;
   gap: 2rem;
+}
+.loading {
+  font-size: 120px;
+  color: white;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
